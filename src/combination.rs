15fe6dc1da4ca7e20b56d61db97cfc41b182d7fb
jibuntu <i.am.tomotatu@gmail.com>
2019-07-09 08:34:22 +0900

    CombIteratorの処理方法を変更

--- src/combination.rs
+++ src/combination.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 pub struct Combination<T> {
     data_list: Vec<T>,
     length_of_combination: usize
@@ -18,44 +20,49 @@ impl<T> Combination<T> {
 
 pub struct CombIterator<'a, T> {
     comb: &'a Combination<T>,
-    keys_list: Vec<Vec<usize>>
+    keys_list: Vec<usize>,
+    keys_list_len: usize,
+    result: Rc<Vec<&'a T>>
 }
 
 impl<'a, T> CombIterator<'a, T> {
     fn new(comb: &'a Combination<T>) -> CombIterator<'a, T> {
         let mut keys: Vec<usize> = (0..comb.data_list.len()).collect();
         let mut keys_list = Vec::new();
+        let result = Rc::new(vec![&comb.data_list[0];comb.length_of_combination]);
 
-        for _i in 0..comb.length_of_combination {
-            keys_list.push(keys.clone());
-            keys.pop();
+        for i in 0..comb.length_of_combination {
+            keys_list.push(comb.data_list.len() - i);
         }
+
+        let keys_list_len = keys_list.len();
 
         CombIterator {
             comb,
-            keys_list
+            keys_list,
+            keys_list_len,
+            result
         }
     }
 
-    fn change_keys_list(&mut self) {
-        self.keys_list.last_mut().unwrap().pop();
+    fn _change_keys_list(&mut self) {
+        self.keys_list[self.keys_list_len - 1] -= 1;
+        if self.keys_list[self.keys_list_len - 1] != 0 {
+            return;
+        }
 
-        for i in 1..self.keys_list.len() {
-            let digit = self.keys_list.len() - i;
-            if self.keys_list[digit].len() < self.keys_list.len() - digit {
+        for i in 1..self.keys_list_len {
+            let digit = self.keys_list_len - i;
+            if self.keys_list[digit] < self.keys_list_len - digit {
                 // 親の値を１つ消す
-                self.keys_list[digit - 1].pop();
-                let mut parent = self.keys_list[digit - 1].clone();
-                parent.pop();
-
-                self.keys_list[digit] = parent;
+                self.keys_list[digit - 1] -= 1;
+                // 親の値から１つ少ない数を自身に入れる
+                self.keys_list[digit] = self.keys_list[digit - 1] - 1;
 
                 // 子にも伝える
-                for digit in digit+1..self.keys_list.len() {
-                    if self.keys_list[digit].len() < self.keys_list.len() - digit {
-                        let mut parent = self.keys_list[digit - 1].clone();
-                        parent.pop();
-                        self.keys_list[digit] = parent;
+                for digit in digit+1..self.keys_list_len {
+                    if self.keys_list[digit] < self.keys_list_len - digit {
+                        self.keys_list[digit] = self.keys_list[digit - 1] - 1;
                     }
                 }
             }
@@ -64,56 +71,40 @@ impl<'a, T> CombIterator<'a, T> {
 }
 
 impl<'a, T: 'a> Iterator for CombIterator<'a, T> {
-    type Item = Vec<&'a T>;
+    type Item = Rc<Vec<&'a T>>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut key_list = Vec::new();
-        for keys in &self.keys_list {
-            if let Some(key) = keys.last() {
-                key_list.push(*key);
-            } else {
+        let result = Rc::make_mut(&mut self.result);
+        for (index, key) in self.keys_list.iter().enumerate() {
+            if *key == 0 {
                 return None;
             }
-
+            result[index] = &self.comb.data_list[*key-1];
         }
+        self._change_keys_list();
 
-        // keys_listを元に値のリストを作成
-        let mut value_list = Vec::new();
-        for key in key_list {
-            value_list.push(&self.comb.data_list[key]);
-        }
-
-        self.change_keys_list();
-
-        Some(value_list)
+        Some(Rc::clone(&self.result))
     }
-
 }
 
 #[test]
 fn test_comb_iterator() {
-    let data_list = vec![
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30
-    ];
-    let comb = Combination::new(data_list, 5);
-    //let mut iter = comb.iter();
+    let data_list = vec![1, 2, 3, 4, 5];
+    let comb = Combination::new(data_list, 3);
 
-    for c in comb.iter() {
-    }
+    let mut iter = comb.iter();
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &4, &3])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &4, &2])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &4, &1])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &3, &2])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &3, &1])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&5, &2, &1])));
 
-    // 0.6s
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
-    //println!("{:?}", iter.next());
+    assert_eq!(iter.next(), Some(Rc::new(vec![&4, &3, &2])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&4, &3, &1])));
+    assert_eq!(iter.next(), Some(Rc::new(vec![&4, &2, &1])));
 
+    assert_eq!(iter.next(), Some(Rc::new(vec![&3, &2, &1])));
+
+    assert_eq!(iter.next(), None);
 }
