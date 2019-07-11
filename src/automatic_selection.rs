d1673f7cce00060a21f743053712716284f4e238
jibuntu <i.am.tomotatu@gmail.com>
2019-07-11 16:44:56 +0900

    automatic_selection関数を作成

--- src/automatic_selection.rs
+++ src/automatic_selection.rs
@@ -6,10 +6,43 @@ use clap::ArgMatches;
 use serde_json::Value;
 
 use crate::food_table::FoodTable;
-use crate::food::{FoodData, Food};
+use crate::food::Food;
+use crate::food::food_data::FoodData;
 use crate::parse_json::{Body, parse_body, parse_json};
 use crate::kijun::Kijun;
 use crate::combination::Combination;
+
+fn color(text: &str, style: &str) -> String {
+    let mut colored_text = String::new();
+    let unset_style = "\x1b[0m";
+
+    for c in style.chars() {
+        let color_style = match c {
+            'd' => "\x1b[30m",
+            'r' => "\x1b[31m",
+            'g' => "\x1b[32m",
+            'y' => "\x1b[33m",
+            'b' => "\x1b[34m",
+            'm' => "\x1b[35m",
+            'c' => "\x1b[36m",
+            'w' => "\x1b[37m",
+            'D' => "\x1b[90m",
+            'R' => "\x1b[91m",
+            'G' => "\x1b[92m",
+            'Y' => "\x1b[93m",
+            'B' => "\x1b[94m",
+            'M' => "\x1b[95m",
+            'C' => "\x1b[96m",
+            'W' => "\x1b[97m",
+            '+' => "\x1b[1m",
+            _ => continue
+        };
+
+        colored_text += color_style;
+    }
+
+    colored_text + text + unset_style
+}
 
 pub fn automatic_selection(matches: &ArgMatches) -> Result<(), String> {
     let path = match matches.value_of("file") {
@@ -40,8 +73,7 @@ pub fn automatic_selection(matches: &ArgMatches) -> Result<(), String> {
             None => return Err(format!("{}番の食材はありません。JSONの値が間違っています", &parsed_food.number))
         };
 
-        food.set_weight(parsed_food.weight.unwrap_or(100.0));
-        food.set("重量", FoodData::String(parsed_food.weight.unwrap_or(100.0).to_string()));
+        food = food.change_weight(parsed_food.weight.unwrap_or(100.0)).unwrap();
         food_table.add(food);
     }
 
@@ -53,17 +85,26 @@ pub fn automatic_selection(matches: &ArgMatches) -> Result<(), String> {
                            parsed_data.body.gender,
                            parsed_data.body.pal);
 
-    // まずは、総当たりの組み合わせを表示する
-    let mut keys: Vec<_> = food_table.iter().map(|(key, food)| key.to_string()).collect();
+    let keys: Vec<_> = food_table.iter().map(|(key, food)| key.to_string()).collect();
 
-    //all_food_comb(keys, next_result, 0);
-    //all_food_comb(keys, 2, 0);
-    //all_food_comb_test(vec![1, 2, 3, 4, 5], vec![], 2, 0);
+    let mut food_table_list: Vec<(f32, FoodTable)> = Vec::new();
+    let comb = Combination::new(keys, 5);
+
+    for key_list in comb.iter() {
+        let keys: Vec<&str> = key_list.iter().map(|key| key.as_str()).collect();
+        let ft = food_table.get_list(&keys);
+        let percentage = ft.percentage_of_kijun(&kijun).unwrap();
+        food_table_list.push((percentage, ft));
+    }
+
+    food_table_list.sort_by(|(p, f), (p2, f2)| p2.partial_cmp(&p).unwrap());
 
 
-    let mut comb = Combination::new(vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20], 5);
-
-    let mut iter = comb.iter();
+    for (index, (percentage, ft)) in food_table_list.iter().take(5).enumerate() {
+        println!("{}", color(&format!("[{}] 摂取基準の達成率: {}", index+1, percentage), "g+"));
+        ft.print_with_sum_and_kijun(&list, &kijun);
+        println!();
+    }
 
     println!("[automatic selection]");
 
