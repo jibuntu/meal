f8ab39d4c9c2e1098401786150c6f8728b24afa1
jibuntu <i.am.tomotatu@gmail.com>
2019-07-23 01:45:05 +0900

    meal.jsのインデントを修正

--- nodejs/meal.js
+++ nodejs/meal.js
@@ -2,71 +2,71 @@ const fs = require("fs")
 const spawn = require("child_process").spawnSync
 
 function Json() {
-    this.data = {}
-    this.path = ""
-    
-    this.foods = (foods) => {
-      this.data["foods"] = foods.data_list
+  this.data = {}
+  this.path = ""
+  
+  this.foods = (foods) => {
+    this.data["foods"] = foods.data_list
+    return this
+  }
+
+  this.body = (body) => {
+    this.data["body"] = body.data
+    return this
+  }
+
+  this.name_list = (type) => {
+    if(type == "摂取基準") {
+      this.data["name_list"] = [
+        "食品番号", "食品名", "価格", "廃棄率", "重量", "エネルギー", "たんぱく質",
+        "脂質", "飽和脂肪酸", "多価不飽和脂肪酸", "炭水化物", "食物繊維総量", 
+        "レチノール活性当量", "ビタミンD", "α-トコフェロール", 
+        "ビタミンK", "ビタミンB1", "ビタミンB2", "ナイアシン", 
+        "ビタミンB6", "ビタミンB12", "葉酸", "パントテン酸", 
+        "ビオチン", "ビタミンC", "ナトリウム", "カリウム", 
+        "カルシウム", "マグネシウム", "リン", "鉄", "亜鉛", "銅", 
+        "マンガン", "ヨウ素", "セレン", "クロム", "モリブデン"
+      ]
+    }
+    return this
+  }
+
+  this.combination = (list) => {
+    if(Array.isArray(list) === false) {
+      this.data["combination"] = [list]
       return this
     }
+    this.data["combination"] = list
+    return this
+  }
 
-    this.body = (body) => {
-      this.data["body"] = body.data
-      return this
-    }
+  this.comb = (list) => {
+    return this.combination(list)
+  }
 
-    this.name_list = (type) => {
-      if(type == "摂取基準") {
-        this.data["name_list"] = [
-          "食品番号", "食品名", "価格", "廃棄率", "重量", "エネルギー", "たんぱく質",
-          "脂質", "飽和脂肪酸", "多価不飽和脂肪酸", "炭水化物", "食物繊維総量", 
-          "レチノール活性当量", "ビタミンD", "α-トコフェロール", 
-          "ビタミンK", "ビタミンB1", "ビタミンB2", "ナイアシン", 
-          "ビタミンB6", "ビタミンB12", "葉酸", "パントテン酸", 
-          "ビオチン", "ビタミンC", "ナトリウム", "カリウム", 
-          "カルシウム", "マグネシウム", "リン", "鉄", "亜鉛", "銅", 
-          "マンガン", "ヨウ素", "セレン", "クロム", "モリブデン"
-        ]
-      }
-      return this
-    }
+  this.path = (path) => {
+    this.path = path
+    return this
+  }
 
-    this.combination = (list) => {
-      if(Array.isArray(list) === false) {
-        this.data["combination"] = [list]
-        return this
-      }
-      this.data["combination"] = list
-      return this
-    }
+  this.write = () => {
+    let json_data = JSON.stringify(this.data)
+    let file = fs.openSync(this.path, "w")
+    fs.writeSync(file, json_data, "uft8")
+    return this
+  }
 
-    this.comb = (list) => {
-      return this.combination(list)
-    }
+  this.automaticSelection = () => {
+    let result = spawn("meal", ["a", this.path])
+    console.log(result.stdout.toString())
+    return this
+  }
 
-    this.path = (path) => {
-      this.path = path
-      return this
-    }
-
-    this.write = () => {
-      let json_data = JSON.stringify(this.data)
-      let file = fs.openSync(this.path, "w")
-      fs.writeSync(file, json_data, "uft8")
-      return this
-    }
-
-    this.automaticSelection = () => {
-      let result = spawn("meal", ["a", this.path])
-      console.log(result.stdout.toString())
-      return this
-    }
-
-    this.calc = () => {
-      let result = spawn("meal", ["c", this.path])
-      console.log(result.stdout.toString())
-      return this
-    }
+  this.calc = () => {
+    let result = spawn("meal", ["c", this.path])
+    console.log(result.stdout.toString())
+    return this
+  }
 }
 
 function Body() {
