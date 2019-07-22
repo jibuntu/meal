51a8570ce005ec3ece9b27555663ece56af53e74
jibuntu <i.am.tomotatu@gmail.com>
2019-07-23 02:34:38 +0900

    test.jsのバグを修正

--- nodejs/test.js
+++ nodejs/test.js
@@ -1,9 +1,7 @@
 let meal = require("./meal")
 
 let command = new meal.Command()
-let json = new meal.Json()
 let body = new meal.Body()
-let foods = new meal.Foods()
 
 body
   .age(20)
@@ -13,24 +11,24 @@ body
   .pal(1)
   .days(2)
 
-foods
+let foods = new meal.Foods()
   .food("01001").w(200)
   .food("01002").w([10, 20, 30])
   .food("01003").w(200).w(300)
   .food("01004").w(20).p(30)
   .food("01004").w(50).p(100)
 
-//json
-//  .body(body)
-//  .foods(foods)
-//  .name_list("摂取基準")
-//  .comb([3, 4, 5])
-//  .path("./test.json")
-//  .write()
+let json = new meal.Json()
+  .body(body)
+  .foods(foods)
+  .name_list("摂取基準")
+  .comb([3, 4, 5])
+  .path("./test.json")
+  .write()
 
 foods = foods.changeWeight(0.5)
 
-json
+json = new meal.Json()
   .body(body)
   .foods(foods)
   .name_list("摂取基準")
